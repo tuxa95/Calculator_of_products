a94be4053f329c231dd95bb74e87223618db8efe
Katherine <43581502+tuxa95@users.noreply.github.com>
2018-12-06 14:40:48 +0300

    conf
    create conflict

--- src/calculator_of_products/Calculator_of_products.java
+++ src/calculator_of_products/Calculator_of_products.java
@@ -15,7 +15,7 @@ public class Calculator_of_products {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic heregg
     }
     
 }
